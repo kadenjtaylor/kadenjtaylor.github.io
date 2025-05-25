bcde3e6122595618651af0645781205f94e2172c
Kaden Taylor <kadenjtaylor@gmail.com>
2025-05-24 18:56:15 -0700

    Added some thoughts on how to proceed

--- src/Styles.elm
+++ src/Styles.elm
@@ -4,6 +4,10 @@ import Domain exposing (Msg)
 import Html exposing (Attribute)
 import Html.Attributes exposing (style)
 
+-- I like the idea of tying the classes in the styles.css file to explicit calls in this file
+-- that way I have a way to keep track of which elements of the styles.css file are being used where 
+-- but I think the way I might wanna do that enventually involves abstracting the classes away entirely
+-- and just having custom elements which are just classed-up divs
 
 centeredBlock : String -> List (Attribute Msg)
 centeredBlock width =
